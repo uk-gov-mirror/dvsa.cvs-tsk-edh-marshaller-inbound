0da88835b87a80dfef18c04c3cb2eba3fc2c78df
khodder <khodder@gmail.com>
2020-05-12 14:25:10 +0100

    CVSB-10776 - standardise functions

--- src/functions/updateTestStation.ts
+++ src/functions/updateTestStation.ts
@@ -1,12 +1,13 @@
 import {APIGatewayProxyEvent, Handler} from "aws-lambda";
 import {ERROR, TARGET, CHANGE} from "../models/enums";
 import {MarshallingService} from "../services/MarshallingService";
+import {HTTPResponse} from "../models/HTTPResponse";
 
 export const updateTestStation: Handler = async (event: APIGatewayProxyEvent) => {
   const payload = event.body;
   const testStationId = event.pathParameters?.testStationId;
   const headers = event.headers;
-  console.log("Trace Header: ", headers["X-Amzn-Trace-Id"]);
+  console.log("Trace Header: ", headers?.["X-Amzn-Trace-Id"]);
 
   if (!payload) {
     throw new Error(ERROR.INVALID_PAYLOAD);
@@ -17,5 +18,7 @@ export const updateTestStation: Handler = async (event: APIGatewayProxyEvent) =>
   }
 
   const marshallingService = new MarshallingService();
-  return await marshallingService.processRequest(payload, TARGET.TEST_STATIONS, CHANGE.UPDATE, testStationId)
+  return await marshallingService.processRequest(payload, TARGET.TEST_STATIONS, CHANGE.UPDATE, testStationId).then(() => {
+    return new HTTPResponse(202, undefined);
+  });
 }
